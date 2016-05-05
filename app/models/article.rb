79e65df89e575b3d8956706fc4e5b96c872d89eb
Anthony Hual <hual.anthony@gmail.com>
2016-05-05 19:46:46 +0000

    Create articles table, model, and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+   
+   #must add validations to make sure empty records aren't created
+   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+   validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+   
+end
