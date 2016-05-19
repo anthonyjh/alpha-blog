a48c28a43524a785e8b952d5f1b573472ea8c3ec
Anthony Hual <hual.anthony@gmail.com>
2016-05-19 00:51:03 +0000

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,11 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
+  end
+  
 end
