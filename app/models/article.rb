aed0c8808fda2c5fbafd842c03e8f87f87b97ad7
Anthony Hual <hual.anthony@gmail.com>
2016-06-03 20:36:34 +0000

    Updated length of articles, modified navbar

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
    
    #must add validations to make sure empty records aren't created
    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-   validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+   validates :description, presence: true, length: { minimum: 10, maximum: 999999 }
    validates :user_id, presence: true
    
 end
